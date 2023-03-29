313f52ad5d7877f89ef5c2c258b326833107b9ec
Dobrowydka <andrikchannelyou@gmail.com>
2023-03-29 16:11:38 +0300

    test: create client and server tests

---
+++ src/test/java/ru/clevertec/concurrency/model/ServerTest.java
@@ -0,0 +1,42 @@
+package ru.clevertec.concurrency.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.clevertec.concurrency.data.Request;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class ServerTest {
+    private Server server;
+
+    @BeforeEach
+    void setUp() {
+        server = new Server();
+    }
+
+    @Test
+    void checkReceivedDataList_shouldBeEqualToClientsDataList() throws InterruptedException {
+        //given
+        int requestsNumber = 1000;
+        List<Integer> clientsInitialData = new ArrayList<>(requestsNumber);
+        for (int i = 1; i <= requestsNumber; i++) {
+            clientsInitialData.add(i);
+        }
+
+        //when
+        for (Integer i : clientsInitialData) {
+            server.processRequest(new Request(i));
+        }
+
+        List<Integer> serverReceivedData = server.getReceivedData();
+
+        //then
+        assertEquals(clientsInitialData, serverReceivedData);
+    }
+}
